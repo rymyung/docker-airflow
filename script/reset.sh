767f7a3323ca6c1d10b3832dac91b4f72af9c68f
rymyung <rymyung@hanmail.net>
2021-06-13 21:52:22 +0900

    Upload reset shee script

--- script/reset.sh
+++ script/reset.sh
@@ -1,13 +1,24 @@
 echo "============ stop containers"
 docker stop docker-airflow_webserver_1
+docker stop docker-airflow_flower_1
+docker stop docker-airflow_scheduler_1
+docker stop docker-airflow_worker_1
+docker stop docker-airflow_init_1
+docker stop docker-airflow_redis_1
 docker stop docker-airflow_postgres_1
 
 echo "============ remove containers"
 docker rm docker-airflow_webserver_1
+docker rm docker-airflow_flower_1
+docker rm docker-airflow_scheduler_1
+docker rm docker-airflow_worker_1
+docker rm docker-airflow_init_1
+docker rm docker-airflow_redis_1
 docker rm docker-airflow_postgres_1
 
 echo "============ remove image"
-docker rmi docker-airflow_webserver
+docker rmi sist/airflow:2.1.0
+docker build -t sist/airflow:2.1.0 .
 
 echo "============ remove & create directory"
 rm -r airflow
@@ -19,7 +30,7 @@ mkdir pgdata
 echo "============ check containers"
 docker ps -a
 
-echo "============ check images"
-docker images
+#echo "============ check images"
+#docker images
 
 echo "============ done"
